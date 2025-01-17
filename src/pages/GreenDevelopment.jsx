52fae97299166f844c53009c505e7bc5c108330f
tawiah <tawiahin4k@gmail.com>
2025-01-17 11:00:56 +0000

    Refactor GreenDevelopment component layout and enhance community focus section

--- src/pages/GreenDevelopment.jsx
+++ src/pages/GreenDevelopment.jsx
@@ -11,18 +11,50 @@ import { ourGoals } from "../constants";
 const GreenDevelopment = () => {
   return (
     <>
-      <Hero2 text={'Green Development'} img={'/green-development.jpeg'} />
+      <Hero2 text={"Green Development"} img={"/green-development.jpeg"} />
 
       {/* Community Focus */}
-      <WriteUp
-        title={"community focus"}
-        text={
-          "At XYZ Ltd., we believe in the transformative power of education and community empowerment. Our focus is on supporting and uplifting the most vulnerable groups, particularly women in rural areas, who are often the backbone of agricultural communities but lack the necessary resources and opportunities. We provide training in sustainable farming techniques, business development, and financial literacy, ensuring that the community can thrive independently."
-        }
-      />
+      <section className="bg-white mt-6 lg:mt-14">
+        <div className="container px-6 py-5 mx-auto">
+          <div className="justify-center text-center items-center">
+            <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl">
+              community focus
+            </h1>
 
-      {/* Horizontal Line */}
-      <HorizontalLine />
+            <BrokenLines />
+          </div>
+          <div className="lg:flex lg:items-center">
+            <div className="w-full space-y-12 lg:w-1/3 ">
+              <div className="md:flex md:items-start md:-mx-4">
+                <div className="mt-4 md:mx-4 md:mt-0">
+                  <p className="mt-3 text-gray-500 text-lg">
+                    At XYZ Ltd., we believe in the transformative power of
+                    education and community empowerment. Our focus is on
+                    supporting and uplifting the most vulnerable groups,
+                    particularly women in rural areas, who are often the
+                    backbone of agricultural communities but lack the necessary
+                    resources and opportunities. We provide training in
+                    sustainable farming techniques, business development, and
+                    financial literacy, ensuring that the community can thrive
+                    independently.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="hidden lg:flex lg:items-center lg:w-1/2 lg:justify-center">
+              <img
+                className="w-[28rem] h-[28rem] object-cover xl:w-[34rem] xl:h-[34rem] rounded-full"
+                src="/plant.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+
+          {/* Horizontal Line */}
+          <HorizontalLine />
+        </div>
+      </section>
 
       {/* Green Development Feature Section  */}
       <section className="py-12 sm:py-16 lg:py-20">
