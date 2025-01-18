42bfc47987dadfb40bf7d85ffe153b63c3f2ef11
tawiah <tawiahin4k@gmail.com>
2025-01-18 20:45:02 +0000

    :poop:

--- src/pages/ProductandServices.jsx
+++ src/pages/ProductandServices.jsx
@@ -9,8 +9,7 @@ import { featureProducts, productsItem } from "../constants";
 // import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 
-import {motion} from 'framer-motion'
-
+import { motion } from "framer-motion";
 
 const ProductandServices = () => {
   return (
