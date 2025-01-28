beba30d47ff5328f514348f995ca0d978b941362
tawiah <tawiahin4k@gmail.com>
2025-01-28 14:20:56 +0000

    ➕ Add @vercel/analytics dependency and integrate analytics component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 /** @format */
 
+
 import { BrowserRouter as Router, Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
 import AnimatedRoute from "./components/AnimatedRoute";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Outlet />
         <AnimatedRoute />
       <Footer />
+      <Analytics />
     </Router>
   );
 }
